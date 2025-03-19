f66a829d3e188be4b608ef5a3a483cd39925cacb
zivmax <zivmax@foxmail.com>
2025-03-19 20:55:15 +0800

    update schema.org URLs to use HTTPS in site overview and image wrapping

--- themes/next/source/dist/music.js
+++ themes/next/source/dist/music.js
@@ -6,8 +6,8 @@ const ap = new APlayer({
 	 {
 		name: 'secret base ~君がくれたもの~',
 		artist: '茅野愛衣 / 戸松遥 / 早見沙織',
-		url: 'http://music.163.com/song/media/outer/url?id=33911781.mp3',
-		cover: 'http://p1.music.126.net/daZcHVIJicL3wXJWMIjAng==/7926379325753633.jpg?',
+		url: 'https://music.163.com/song/media/outer/url?id=33911781.mp3',
+		cover: 'https://p1.music.126.net/daZcHVIJicL3wXJWMIjAng==/7926379325753633.jpg?',
 	    lrc: 'images/.lrc' #歌词播放并不好看，所以没有使用
 	 },
     ]
